65323ccf5886bd1a54643f06f0890585f19525d1
Sanjit Juneja <sanjit.j55@gmail.com>
2021-07-08 21:59:34 -0500

    Added hide-items  and fade-in-text functionality

--- app.js
+++ app.js
@@ -1,52 +1,81 @@
-// const btn = document.querySelector('.switch-btn');
-const video = document.querySelector('.video-container');
+/* Retrieving Elements */
+let preloader = document.querySelector('.preloader');
+let video = document.querySelector('.video-container');
+let playPause = document.querySelector('.btn-play-pause');
+let shuffle = document.querySelector('.btn-shuffle');
+let quote = document.getElementById('quoteDisplay');
+let randVideo = document.getElementById('random-video');
+let hideButton = document.querySelector('.btn-hide-items');
+let appTitle = document.querySelector('.app-title');
+let signature = document.querySelector('.signature');
+   
 
-// btn.addEventListener("click", function () {
-//     if (!btn.classList.contains("slide")) {
-//       btn.classList.add("slide");
-//       video.pause();
-//     } else {
-//       btn.classList.remove("slide");
-//       video.play();
-//     }
-//   });
-
-const preloader = document.querySelector('.preloader');
-
+/* Hide Preloader once page loads */
 window.addEventListener('load', function() {
     preloader.classList.add("hide-preloader");
-    displayVideo();
-    displayQuote(); 
+    displayVideo(); 
+    displayQuote();
 })
 
-const playPause = document.querySelector('.btn-play-pause');
+/* Display Random Quote */
+function displayQuote() {
+    let randomNumber = Math.floor(Math.random() * (quotes.length));
+    quote.innerHTML = quotes[randomNumber];
+}
 
+/* Display Random Video */
+function displayVideo() {
+    randVideo.src = "./videos/" + Math.floor(Math.random() * 133) + ".mp4";
+}
+
+/* Play/Pause Button EventListener */
 playPause.addEventListener('click', function() {
     if (playPause.innerHTML === 'Pause Video') {
         video.pause();
-        playPause.innerHTML = 'Resume Video';
+        playPause.innerHTML = 'Play Video';
     } else {
         video.play();
         playPause.innerHTML = 'Pause Video';
     }
 });
 
-const shuffle = document.querySelector('.btn-shuffle');
-
+/* Shuffle Button EventListener */
 shuffle.addEventListener('click', function() {
     displayVideo();
     displayQuote();
+    if (playPause.innerHTML === 'Play Video') {
+        playPause.innerHTML = 'Pause Video';
+    }
 });
 
-function displayQuote() {
-    const randomNumber = Math.floor(Math.random() * (quotes.length));
-    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
-}
+/* Hide Items Button EventListener */
+hideButton.addEventListener('click', function() {
+    if (hideButton.innerHTML === "Hide Items") {
+        shuffle.classList.remove('fade-in-text');
+        playPause.classList.remove('fade-in-text');
+        appTitle.classList.remove('fade-in-text');
+        signature.classList.remove('fade-in-text');
+        shuffle.style.display = "none";
+        playPause.style.display = "none";
+        appTitle.style.display = "none";
+        signature.style.display = "none";
+        hideButton.innerHTML = "Show Items"
+        hideButton.classList.add("btn-hide-items-hidden");
+    } else {
+        hideButton.classList.remove("btn-hide-items-hidden");
+        shuffle.classList.add('fade-in-text');
+        playPause.classList.add('fade-in-text');
+        appTitle.classList.add('fade-in-text');
+        signature.classList.add('fade-in-text');
+        shuffle.style.display = "block";
+        playPause.style.display = "block";
+        appTitle.style.display = "block";
+        signature.style.display = "block";
+        hideButton.innerHTML = "Hide Items"
+    }
+});
 
-function displayVideo() {
-    document.getElementById("random-video").src = "./videos/" + Math.floor(Math.random() * 2) + ".mp4";
-}
-
+/* Quotes Gathered From Internet */
 const quotes = [
     "I have always believed that each man makes his own happiness and is responsible for his own problems. It is a simple philosophy.",
     "When we have respect for ourselves and others, we gravitate towards connections that encourage that.",
@@ -95,5 +124,59 @@ const quotes = [
     "Stay hungry, stay foolish.",
     "I want to put a ding in the universe.",
     "And no, we don't know where it will lead. We just know there's something much bigger than any of us here.",
-    "If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it. And, like any great relationship, it just gets better and better as the years roll on."
+    "If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it. And, like any great relationship, it just gets better and better as the years roll on.",
+    "Happiness is when what you think, what you say, and what you do are in harmony.",
+    "Some cause happiness wherever they go; others whenever they go.",
+    "Perfection is not attainable, but if we chase perfection we can catch excellence.",
+    "We know what we are, but know not what we may be.",
+    "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+    "Keep your face always toward the sunshine - and shadows will fall behind you.",
+    "It is never too late to be what you might have been.",
+    "Opportunity doesn't knock, build a door.",
+    "Your present circumstances don't determine where you can go; they merely determine where you start.",
+    "The Best Way To Get Started Is To Quit Talking And Begin Doing.",
+    "The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.",
+    "Don’t Let Yesterday Take Up Too Much Of Today.",
+    "If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.",
+    "People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.",
+    "Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough.",
+    "Knowing Is Not Enough; We Must Apply. Wishing Is Not Enough; We Must Do.",
+    "Imagine Your Life Is Perfect In Every Respect; What Would It Look Like?",
+    "We Generate Fears While We Sit. We Overcome Them By Action.",
+    "Whether You Think You Can Or Think You Can’t, You’re Right.",
+    "Security Is Mostly A Superstition. Life Is Either A Daring Adventure Or Nothing.",
+    "The Man Who Has Confidence In Himself Gains The Confidence Of Others.",
+    "The Only Limit To Our Realization Of Tomorrow Will Be Our Doubts Of Today.",
+    "Creativity Is Intelligence Having Fun.",
+    "What You Lack In Talent Can Be Made Up With Desire, Hustle And Giving 110% All The Time.",
+    "Do What You Can With All You Have, Wherever You Are.",
+    "To See What Is Right And Not Do It Is A Lack Of Courage.",
+    "The Future Belongs To The Competent. Get Good, Get Better, Be The Best!",
+    "For Every Reason It’s Not Possible, There Are Hundreds Of People Who Have Faced The Same Circumstances And Succeeded.",
+    "Things Work Out Best For Those Who Make The Best Of How Things Work Out.",
+    "The Only Way To Do Great Work Is To Love What You Do. If You Haven’t Found It Yet, Keep Looking. Don’t Settle.",
+    "You Don’t Have To Be Great To Start, But You Have To Start To Be Great.",
+    "A Clear Vision, Backed By Definite Plans, Gives You A Tremendous Feeling Of Confidence And Personal Power.",
+    "There Are No Limits To What You Can Accomplish, Except The Limits You Place On Your Own Thinking.",
+    "No matter what people tell you, words and ideas can change the world.",
+    "I am not afraid of tomorrow, for I have seen yesterday and I love today!",
+    "You can never plan the future by the past.",
+    "Take into account that great love and great achievements involve great risk.",
+    "Lost time is never found again.",
+    "He that respects himself is safe from others.",
+    "It is the province of knowledge to speak, and it is the privilege of wisdom to listen.",
+    "Don’t take life too seriously. You’ll never get out of it alive.",
+    "If you talk to a man in a language he understands, that goes to his head. If you talk to him in his language, that goes to his heart.",
+    "Nothing that you have not given away will ever be really yours.",
+    "Singleness of purpose is one of the chief essentials for success in life, no matter what may be one’s aim.",
+    "The two most important days in your life are the day you are born and the day you find out why.",
+    "There are many ways of going forward, but only one way of standing still.",
+    "When a thing is done, it’s done. Don’t look back. Look forward to your next objective.",
+    "Use what talents you possess; the woods would be very silent if no birds sang there except those that sang best.",
+    "In the end, it’s not the years in your life that count. It’s the life in your years.",
+    "Life is a mountain. Your goal is to find your path, not to reach the top.",
+    "If you spend too much time thinking about a thing, you’ll never get it done.",
+    "Keep your eyes on the stars and your feet on the ground.",
+    "Change your thoughts and you change your world.",
+    "Life can only be understood backwards; but it must be lived forwards."
 ];
